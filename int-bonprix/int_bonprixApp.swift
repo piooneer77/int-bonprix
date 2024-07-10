588bfe72457ea4772b201efd9a228d1e52a301bf
Karem Gohar <piooneer77@me.com>
2024-07-10 13:46:30 +0200

    Initial Commit

---
+++ int-bonprix/int_bonprixApp.swift
@@ -0,0 +1,17 @@
+//
+//  int_bonprixApp.swift
+//  int-bonprix
+//
+//  Created by Halawany on 10.07.24.
+//
+
+import SwiftUI
+
+@main
+struct int_bonprixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
